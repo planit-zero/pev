4ccc5a4f327361d10b737e26fc4706a4413e427c
chuck-planit <chuck@planit.ai>
2023-08-03 09:25:06 +0900

    first commit

---
+++ src/main/java/ai/planit/pev/PevApplication.java
@@ -0,0 +1,13 @@
+package ai.planit.pev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PevApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ai/planit/pev/PevApplicationTests.java
@@ -0,0 +1,13 @@
+package ai.planit.pev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
