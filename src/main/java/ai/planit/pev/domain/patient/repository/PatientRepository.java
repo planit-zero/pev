f41470867211263ff4227927cc0655c58dd27734
chuck-planit <chuck@planit.ai>
2023-08-07 15:16:51 +0900

    engineering: JPA 관련 소스 제거

--- src/main/java/ai/planit/pev/domain/patient/repository/PatientRepository.java
+++
@@ -1,7 +0,0 @@
-package ai.planit.pev.domain.patient.repository;
-
-import ai.planit.pev.domain.patient.entity.Patient;
-
-public interface PatientRepository {
-    Patient findPatientByPtNo(String ptNo);
-}
--- src/main/java/ai/planit/pev/domain/record/repository/RecordRepository.java
+++
@@ -1,9 +0,0 @@
-package ai.planit.pev.domain.record.repository;
-
-import ai.planit.pev.domain.record.dto.Certificate;
-
-import java.util.List;
-
-public interface RecordRepository {
-    List<Certificate> findAllCertificateByPtNo(String ptNo);
-}
