4029b77cb9ea360961122ee7553caa5d15123341
chuck-planit <chuck@planit.ai>
2023-09-19 13:34:04 +0900

    chore: 일부 주석 추가

--- src/main/java/ai/planit/pev/domain/record/service/RecordServiceImpl.java
+++ src/main/java/ai/planit/pev/domain/record/service/RecordServiceImpl.java
@@ -21,9 +21,11 @@ public class RecordServiceImpl implements RecordService {
 
     private final RecordListDAO recordListDAO;
 
+    /** {@inheritDoc} */
     public List<Record.Response> getRecordList(HttpSession session, Record.Request request) {
         String pid = (String) session.getAttribute("pev-pid");
 
+        // 세션에 저장된 환자병록번호가 없을 경우 예외 처리한다.
         if (PevStringUtil.isStringEmpty(pid)) {
             throw new BaseException(ErrorType.PID_NOT_FOUND_IN_SESSION);
         }
@@ -33,12 +35,18 @@ public class RecordServiceImpl implements RecordService {
         return getRecordListByTargets(request);
     }
 
+    /**
+     * 기록 종류별 기록 목록 가져오기
+     *
+     * @param request 조회할 기록 목록의 상세 조건
+     * @return 데이터베이스에서 조회한 기록 목록
+     */
     private List<Record.Response> getRecordListByTargets(Record.Request request) {
         List<Record.Response> recordList = new ArrayList<>();
 
         List<String> searchTargetList = Arrays.asList(request.getSearchTargets());
 
-        // 진료
+        // 진료기록
         List<String> medicalRecordTargets = searchTargetList
                 .stream()
                 .filter(target -> target.startsWith("D0"))
@@ -53,14 +61,15 @@ public class RecordServiceImpl implements RecordService {
 //            recordList.addAll(recordListDAO.getOrderRecordList(request));
 //        }
 
-        // 검사
+        // TODO: 검사기록 목록 연동
 
-        // 간호
+        // TODO: 간호기록 목록 연동
 
-        // 스캔
+        // TODO: 스캔자료 목록 연동
 
-        // 특성화
+        // TODO: 특성화기록 목록 연동
 
+        // 조건에 따라 여러 기록을 조회하기 때문에 모든 조회가 끝난 후 한번에 정렬한다.
         recordList = recordList
                 .stream()
                 .sorted(Comparator.comparing(Record.Response::getWritingDate).reversed())
@@ -72,7 +81,7 @@ public class RecordServiceImpl implements RecordService {
     private List<Record.Response> getMedicalRecordList(Record.Request request, List<String> searchTargetList) {
         List<Record.Response> medicalRecordList = new ArrayList<>();
 
-        // 수술기록
+        // 진료기록 - 수술기록
         if (searchTargetList.contains("D005")) {
             String[] queryTargets = {"D005"};
             request.setQueryTargets(queryTargets);
@@ -80,7 +89,7 @@ public class RecordServiceImpl implements RecordService {
             medicalRecordList.addAll(recordListDAO.getSurgeryRecordList(request));
         }
 
-        // 퇴원기록
+        // 진료기록 - 퇴원기록
         if (searchTargetList.contains("D006")) {
             String[] queryTargets = {"D006"};
             request.setQueryTargets(queryTargets);
@@ -88,7 +97,7 @@ public class RecordServiceImpl implements RecordService {
             medicalRecordList.addAll(recordListDAO.getDischargeRecordList(request));
         }
 
-        // 타과의뢰
+        // 진료기록 - 타과의뢰
         if (searchTargetList.contains("D007")) {
             String[] queryTargets = {"D007"};
             request.setQueryTargets(queryTargets);
@@ -96,7 +105,7 @@ public class RecordServiceImpl implements RecordService {
             medicalRecordList.addAll(recordListDAO.getRequestRecordList(request));
         }
 
-        // 마취기록, 마취 전 평가
+        // 진료기록 - 마취기록, 마취 전 평가
         if (searchTargetList.contains("D010") || searchTargetList.contains("D011")) {
             String[] queryTargets = {"D010", "D011"};
 
@@ -117,7 +126,7 @@ public class RecordServiceImpl implements RecordService {
             medicalRecordList.addAll(recordListDAO.getAnesthesiaRecordList(request));
         }
 
-        // 진료기록 일반
+        // 진료기록 - 일반
         List<String> generalTypeList = searchTargetList.stream()
                 .filter(type -> !type.equals("D005"))
                 .filter(type -> !type.equals("D006"))
