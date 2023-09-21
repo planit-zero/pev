530f5d91a974b8f216a1502faf64390ed29abcd8
chuck-planit <chuck@planit.ai>
2023-09-21 12:37:28 +0900

    feat: RecordTarget 추가

--- src/main/java/ai/planit/pev/domain/record/constant/RecordTargetType.java
+++ src/main/java/ai/planit/pev/domain/record/constant/RecordTargetType.java
@@ -6,25 +6,32 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum RecordTargetType {
+    // 진료기록
     MEDICAL_RECORD("D0", "진료기록"),
     MEDICAL_SURGERY("D005", "진료기록 - 수술기록"),
     MEDICAL_DISCHARGE("D006", "진료기록 - 퇴원기록"),
     MEDICAL_REQUEST("D007", "진료기록 - 타과의뢰"),
     MEDICAL_ANESTHESIA("D010", "진료기록 - 마취기록"),
     MEDICAL_BEFORE_ANESTHESIA("D011", "진료기록 - 마취전평가"),
+
+    // 처방
     ORDER_RECORD("OR", "처방기록"),
 
+    // 검사
     EXAM_RECORD("EX_", "검사기록"),
     EXAM_PICTURE("EX_PICTURE", "영상검사"),
     EXAM_PATHOLOGY("EX_PATHOLOGY", "병리검사"),
     EXAM_SPECIMEN("EX_SPECIMEN", "검체검사"),
     EXAM_FUNCTION("EX_FUNCTION", "기능검사"),
 
+    // 간호
     NURS_RECORD("NR", "간호기록"),
 
+    // 스캔자료
     SCAN_RECORD("SC", "스캔자료"),
 
-
+    // 특성화기록
+    CHARACTERIZATION_RECORD("CR", "특성화기록"),
     ;
 
 
