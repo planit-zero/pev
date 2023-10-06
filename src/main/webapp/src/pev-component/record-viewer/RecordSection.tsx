fcc2d9df77ba9b77858c9dee28f96ed665546376
chuck-planit <chuck@planit.ai>
2023-10-06 14:50:07 +0900

    modify: 헤더 섹션 italic CSS 추가

--- src/main/webapp/src/pev-component/record-viewer/RecordSection.tsx
+++ src/main/webapp/src/pev-component/record-viewer/RecordSection.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { IRecordSection } from '../../pev-interface/IRecord';
 import { Box } from '@mui/material';
 import RecordEntity from './RecordEntity';
+import { TRecordSection } from '../../pev-type/TRecordSection';
 
 type RecordSectionProps = {
     type: string;
@@ -10,7 +11,12 @@ type RecordSectionProps = {
 
 const RecordSection = (props: RecordSectionProps) => {
     return (
-        <Box display={'flex'} flexDirection={'column'} gap={2}>
+        <Box
+            display={'flex'}
+            flexDirection={'column'}
+            gap={2}
+            sx={{ fontStyle: props.type === TRecordSection.HEADER ? 'italic' : 'normal' }}
+        >
             {props.section.entities.map((entity, idx) => {
                 return <RecordEntity key={idx} sectionType={props.type} entity={entity} />;
             })}
