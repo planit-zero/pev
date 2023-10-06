ef49990a316d5f612d1efa4ed49343ce07152cac
chuck-planit <chuck@planit.ai>
2023-10-06 12:40:40 +0900

    modify: Grid CSS 조정

--- src/main/webapp/src/pev-component/record-finder/condition-finder/ConditionFinderGrid.tsx
+++ src/main/webapp/src/pev-component/record-finder/condition-finder/ConditionFinderGrid.tsx
@@ -3,7 +3,7 @@ import { Box, Button, Divider, Typography } from '@mui/material';
 import { Bookmark } from '@mui/icons-material';
 import { IRecord } from '../../../pev-interface/IRecord';
 import { DataGrid } from 'devextreme-react';
-import { Column, FilterRow, Scrolling, Selection } from 'devextreme-react/data-grid';
+import { Column, Scrolling, Selection } from 'devextreme-react/data-grid';
 import SearchIcon from '@mui/icons-material/Search';
 import { setTargetRecords } from '../../../store/pev-slices/record';
 
@@ -43,17 +43,17 @@ const ConditionFinderGrid = (props: ConditionFinderGridProps) => {
                     showBorders={true}
                     showColumnLines={true}
                     showRowLines={true}
+                    wordWrapEnabled={true}
                     noDataText={''}
                     onSelectionChanged={handleSelectionChanged}
                 >
-                    <FilterRow visible={true} />
-                    <Column dataField={'pactTpNm'} caption={'환자구분'} alignment={'center'} width={100} />
-                    <Column dataField={'itemType'} caption={'항목구분'} alignment={'center'} width={100} />
-                    <Column dataField={'itemNm'} caption={'항목명'} alignment={'left'} />
-                    <Column dataField={'writingDate'} caption={'작성일자'} width={120} alignment={'center'} />
-                    <Column dataField={'writingDeptNm'} caption={'작성과'} width={100} alignment={'center'} />
-                    <Column dataField={'writerNm'} caption={'작성자'} width={85} alignment={'center'} />
-                    <Column dataField={'mdrcWrtStsCdYn'} caption={'서명'} alignment={'center'} width={75} />
+                    <Column dataField={'pactTpNm'} caption={'환자구분'} alignment={'center'} width={50} />
+                    <Column dataField={'itemType'} caption={'항목구분'} alignment={'center'} width={50} />
+                    <Column dataField={'itemNm'} caption={'항목명'} alignment={'left'} width={180} />
+                    <Column dataField={'writingDate'} caption={'작성일자'} width={100} alignment={'center'} />
+                    <Column dataField={'writingDeptNm'} caption={'작성과'} width={100} alignment={'left'} />
+                    <Column dataField={'writerNm'} caption={'작성자'} width={80} alignment={'center'} />
+                    <Column dataField={'mdrcWrtStsCdYn'} caption={'서명'} alignment={'center'} width={60} />
                     <Scrolling mode={'virtual'} />
                     <Selection mode={'multiple'} showCheckBoxesMode={'onClick'} />
                 </DataGrid>
