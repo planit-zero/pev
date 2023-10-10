700d45d3dca2fa4ca716ffe365e76cb56738a19f
chuck-planit <chuck@planit.ai>
2023-10-10 09:13:39 +0900

    feat: 기록지 로딩시 Skeleton 구현

--- src/main/webapp/src/pev-component/record-viewer/RecordSheet.tsx
+++ src/main/webapp/src/pev-component/record-viewer/RecordSheet.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { IRecord } from '../../pev-interface/IRecord';
-import { Paper } from '@mui/material';
+import { Box, Paper, Skeleton } from '@mui/material';
 import { useGetRecordSheetMutation } from '../../pev-service/RecordService';
 import RecordSection from './RecordSection';
 import { TRecordSection } from '../../pev-type/TRecordSection';
@@ -17,14 +17,40 @@ const RecordSheet = (props: RecordSheetProps) => {
     }, []);
 
     return (
-        <Paper sx={{ width: 600, p: 2, mb: 2, borderRadius: 0, display: 'flex', flexDirection: 'column', gap: 3 }}>
-            {recordSheet && (
-                <React.Fragment>
-                    {recordSheet.headerSection && <RecordSection type={TRecordSection.HEADER} section={recordSheet.headerSection} />}
-                    {recordSheet.sections.map((section, idx) => {
-                        return <RecordSection key={idx} type={TRecordSection.BODY} section={section} />;
-                    })}
-                </React.Fragment>
+        <Paper sx={{ width: 600, p: 2, mb: 2, borderRadius: 0 }}>
+            {!isRecordSheetLoading && recordSheet && (
+                <Box sx={{ display: 'flex', flexDirection: 'column', gap: 3 }}>
+                    <React.Fragment>
+                        {recordSheet.headerSection && <RecordSection type={TRecordSection.HEADER} section={recordSheet.headerSection} />}
+                        {recordSheet.sections.map((section, idx) => {
+                            return <RecordSection key={idx} type={TRecordSection.BODY} section={section} />;
+                        })}
+                    </React.Fragment>
+                </Box>
+            )}
+            {isRecordSheetLoading && (
+                <Box display={'flex'} flexDirection={'column'} gap={2}>
+                    <Box>
+                        <Skeleton variant={'text'} width={'50%'} height={40} />
+                    </Box>
+                    <Box>
+                        <Skeleton variant={'text'} width={'30%'} height={30} />
+                        <Skeleton variant={'text'} width={'100%'} height={20} />
+                        <Skeleton variant={'text'} width={'100%'} height={20} />
+                        <Skeleton variant={'text'} width={'100%'} height={20} />
+                        <Skeleton variant={'text'} width={'100%'} height={20} />
+                    </Box>
+                    <Box>
+                        <Skeleton variant={'text'} width={'30%'} height={30} />
+                        <Skeleton variant={'text'} width={'100%'} height={20} />
+                        <Skeleton variant={'text'} width={'100%'} height={20} />
+                        <Skeleton variant={'text'} width={'100%'} height={20} />
+                        <Skeleton variant={'text'} width={'100%'} height={20} />
+                    </Box>
+                    <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+                        <Skeleton variant={'text'} width={'30%'} height={30} />
+                    </Box>
+                </Box>
             )}
         </Paper>
     );
