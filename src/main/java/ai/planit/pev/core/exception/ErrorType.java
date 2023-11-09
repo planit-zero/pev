e82cfbd04a539c0ab52a39121bb9187ef7c979c6
chuck-planit <chuck@planit.ai>
2023-11-09 12:53:12 +0900

    modify: 가명화 관련 오류 처리 텍스트 변경

--- src/main/java/ai/planit/pev/core/exception/ErrorType.java
+++ src/main/java/ai/planit/pev/core/exception/ErrorType.java
@@ -14,6 +14,7 @@ public enum ErrorType {
     // RID
     RID_CONNECTION_TIMEOUT(408, "가명화 시스템에 연결할 수 없습니다."),
     CONVERT_RID_TO_PID_FAILED(500, "환자병록번호 변환에 실패했습니다."),
+    ANN_PROCESS_FAILED(500, "가명화 처리에 실패했습니다."),
 
     // Record
     PID_NOT_FOUND_IN_SESSION(400, "연구별 환자 ID로 환자를 먼저 조회해 주세요."),
