3b1f0d7f7b6c08f74d255c501d9ad898ed82226e
dev-chuck <im.creativeho.me@gmail.com>
2024-01-08 16:20:41 +0900

    modify: 서비스 명칭 변경

--- src/main/webapp/src/layout/MainLayout/Header/index.tsx
+++ src/main/webapp/src/layout/MainLayout/Header/index.tsx
@@ -28,7 +28,7 @@ const Header = () => {
         <Box width={'100%'} display={'flex'} justifyContent={'space-between'} alignItems={'center'} sx={{ pl: '40px' }}>
             <Box component="span" display={'flex'} justifyContent={'flex-start'} alignItems={'center'} gap={1}>
                 <IconReportMedical color={'#3f51b5'} />
-                {!matchDownMd && <Typography sx={{ fontSize: 'h3.fontSize', fontWeight: 'bold' }}>가명화 EMR Viewer</Typography>}
+                {!matchDownMd && <Typography sx={{ fontSize: 'h3.fontSize', fontWeight: 'bold' }}>가명의무기록 뷰어</Typography>}
             </Box>
             {info && (
                 <Box display={'flex'} justifyContent={'flex-start'} alignItems={'center'} gap={1}>
