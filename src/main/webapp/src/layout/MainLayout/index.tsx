6d78e32cd0d532416589307111b181921e074a7e
chuck-planit <chuck@planit.ai>
2024-01-31 10:02:36 +0900

    feat: 메인으로 가는 홈 버튼 추가 closed #44

--- src/main/webapp/src/layout/MainLayout/index.tsx
+++ src/main/webapp/src/layout/MainLayout/index.tsx
@@ -13,7 +13,7 @@ import { useSelector } from 'store';
 import RecordViewer from '../../pev-component/record-viewer/RecordViewer';
 import CommonSnackbar from '../../pev-component/common/CommonSnackbar';
 import * as React from 'react';
-import { IconGridDots, IconMessageReport, IconReportMedical, IconSettings } from '@tabler/icons';
+import { IconGridDots, IconHome, IconMessageReport, IconReportMedical, IconSettings } from '@tabler/icons';
 
 interface MainStyleProps {
     theme: Theme;
@@ -108,6 +108,17 @@ const MainLayout = () => {
             >
                 <Box width={48} height={48} display={'flex'} justifyContent={'center'} alignItems={'center'} sx={{ cursor: 'pointer' }}>
                     <IconGridDots color={'white'} />
+                </Box>
+                <Box
+                    width={48}
+                    height={48}
+                    display={'flex'}
+                    justifyContent={'center'}
+                    alignItems={'center'}
+                    sx={{ cursor: 'pointer' }}
+                    onClick={() => (window.location.href = 'https://supreme.snuh.org')}
+                >
+                    <IconHome color={'white'} />
                 </Box>
                 <Box width={48} height={48} display={'flex'} justifyContent={'center'} alignItems={'center'} sx={{ cursor: 'pointer' }}>
                     <IconReportMedical color={'white'} />
