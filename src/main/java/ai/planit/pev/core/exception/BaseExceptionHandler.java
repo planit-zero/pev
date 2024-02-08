ae07c88f00ef5c3711234d9a472efe64ada61578
chuck-planit <chuck@planit.ai>
2024-02-08 14:36:20 +0900

    fix: SQL 관련 ExceptionHandler 추가

--- src/main/java/ai/planit/pev/core/exception/BaseExceptionHandler.java
+++ src/main/java/ai/planit/pev/core/exception/BaseExceptionHandler.java
@@ -1,14 +1,19 @@
 package ai.planit.pev.core.exception;
 
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.sql.SQLException;
+
 @RestControllerAdvice
 public class BaseExceptionHandler {
     @ExceptionHandler({BaseException.class})
     protected ResponseEntity<?> handleBaseException(Exception e) {
+        System.err.println(e.getMessage());
+
         int status = 500;
         String message = e.getMessage();
 
@@ -22,8 +27,18 @@ public class BaseExceptionHandler {
         return new ResponseEntity<>(error, HttpStatus.valueOf(status));
     }
 
+    @ExceptionHandler({SQLException.class, DataAccessException.class})
+    protected ResponseEntity<?> handleSqlException(Exception e) {
+        System.err.println(e.getMessage());
+
+        Error error = new Error(500, "데이터 조회 중 오류가 발생했습니다.");
+        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
     @ExceptionHandler({Exception.class})
     protected ResponseEntity<?> handleException(Exception e) {
+        System.err.println(e.getMessage());
+
         Error error = new Error(500, e.getMessage());
         return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
     }
