f935d0ec600ae5d758f51051481d22e890b25d88
chuck-planit <chuck@planit.ai>
2024-02-13 13:43:43 +0900

    feat: 이미지 처리용 외부 디렉토리 리소스 연결 closed #62

---
+++ src/main/java/ai/planit/pev/config/resource/ResourceConfiguration.java
@@ -0,0 +1,17 @@
+package ai.planit.pev.config.resource;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ResourceConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/masked_images/**")
+                .addResourceLocations("file:///data/masked_images/")
+                .setCacheControl(CacheControl.noCache());
+    }
+}
