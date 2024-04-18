ae140f05b74a67be2a1bba4c6338cdf3f9667d87
Evan0037 <evan@planit.ai>
2024-04-18 11:11:20 +0900

    fix: 사이드바 변경 #110

--- src/main/webapp/src/pev-component/common/CommonLayout.tsx
+++ src/main/webapp/src/pev-component/common/CommonLayout.tsx
@@ -1,17 +1,19 @@
 import * as React from 'react';
-import { AppBar, Box, Toolbar } from '@mui/material';
+import {AppBar, Box, Toolbar, Typography} from '@mui/material';
 import CommonSnackbar from './CommonSnackbar';
-import { IconGridDots, IconHome, IconMessageReport, IconReportMedical, IconSettings } from '@tabler/icons';
+import { faGrid, faChartMixed, faCircleQuestion, faHouseChimney, faMagnifyingGlass, faAddressCard, faGear } from '@fortawesome/pro-solid-svg-icons';
 import Header from '../../layout/MainLayout/Header';
 import { useSelector } from '../../store';
 import { useTheme } from '@mui/material/styles';
 import { useNavigate } from 'react-router-dom';
 import { useGetTokenInSessionMutation } from '../../pev-service/UserService';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 
 const CommonLayout = () => {
     const theme = useTheme();
     const navigate = useNavigate();
     const { info } = useSelector((state) => state.user);
+    const [isOpen, setIsOpen] = React.useState<boolean>(false);
 
     const header = () => {
         return (
@@ -29,6 +31,14 @@ const CommonLayout = () => {
             .then((data) => {
                 window.location.href = `https://supreme.snuh.org/sso?token=${data.token}`;
             });
+    };
+
+    const getFont = (value: string) => {
+        if (isOpen) return (
+            <Typography fontSize={20} fontWeight={'bold'} color="white" marginLeft={1} display={'flex'} alignItems={'center'}>
+                {value}
+            </Typography>
+        );
     };
 
     return (
@@ -54,58 +64,80 @@ const CommonLayout = () => {
                     position: 'fixed',
                     top: 0,
                     left: 0,
-                    width: 48,
+                    width: isOpen ? 338 : 48,
                     height: '100vh',
                     backgroundColor: '#3f51b5'
                 }}
             >
-                <Box width={48} height={48} display={'flex'} justifyContent={'center'} alignItems={'center'} sx={{ cursor: 'pointer' }}>
-                    <IconGridDots color={'white'} />
+                <Box
+                    height={55}
+                    display={'flex'}
+                    alignItems={'center'}
+                    sx={{ cursor: 'pointer', padding: '0 14px' }}
+                    onClick={() => setIsOpen(!isOpen)}
+                >
+                    <FontAwesomeIcon icon={faGrid} size='xl' style={{ color: 'white' }} />
                 </Box>
                 <Box
-                    width={48}
-                    height={48}
+                    height={55}
                     display={'flex'}
-                    justifyContent={'center'}
                     alignItems={'center'}
-                    sx={{ cursor: 'pointer' }}
+                    sx={{ cursor: 'pointer', padding: '0 14px' }}
                     onClick={goToRex}
                 >
-                    <IconHome color={'white'} />
+                    <FontAwesomeIcon icon={faHouseChimney} size='lg' style={{ color: 'white' }} />
+                    {getFont('SUPREME 2.0')}
                 </Box>
                 <Box
-                    width={48}
-                    height={48}
+                    height={55}
                     display={'flex'}
-                    justifyContent={'center'}
                     alignItems={'center'}
-                    sx={{ cursor: 'pointer' }}
-                    onClick={() => navigate('/')}
+                    sx={{ cursor: 'pointer', padding: '0 14px' }}
+                    onClick={() => window.location.href = 'https://supreme.snuh.org/case-design'}
                 >
-                    <IconReportMedical color={'white'} />
+                    <FontAwesomeIcon icon={faMagnifyingGlass} size='lg' style={{ color: 'white' }} />
+                    {getFont('연구검색')}
                 </Box>
                 <Box
-                    width={48}
-                    height={48}
+                    height={55}
                     display={'flex'}
-                    justifyContent={'center'}
                     alignItems={'center'}
-                    sx={{ cursor: 'pointer' }}
-                    onClick={() => navigate('/report')}
+                    sx={{ cursor: 'pointer', padding: '0 14px' }}
+                    onClick={() => window.location.href = 'https://cohortmarts.snuh.org/'}
                 >
-                    <IconMessageReport color={'white'} />
+                    <FontAwesomeIcon icon={faChartMixed} size='lg' style={{ color: 'white' }} />
+                    {getFont('코호트 마트')}
+                </Box>
+                <Box
+                    height={55}
+                    display={'flex'}
+                    alignItems={'center'}
+                    sx={{ cursor: 'pointer', padding: '0 14px' }}
+                    onClick={() => window.location.href = 'https://rid.snuh.org/'}
+                >
+                    <FontAwesomeIcon icon={faAddressCard} size='lg' style={{ color: 'white' }} />
+                    {getFont('가명화 시스템')}
+                </Box>
+                <Box
+                    height={55}
+                    display={'flex'}
+                    alignItems={'center'}
+                    sx={{ cursor: 'pointer', padding: '0 14px' }}
+                    onClick={() => window.location.href = 'http://172.26.33.22:18081/'}
+                >
+                    <FontAwesomeIcon icon={faCircleQuestion} size='lg' style={{ color: 'white' }} />
+                    {getFont('Help Center')}
                 </Box>
                 {info && info.authCd === 'S' && (
                     <Box
-                        width={48}
-                        height={48}
+                        height={55}
                         display={'flex'}
-                        justifyContent={'center'}
                         alignItems={'center'}
-                        sx={{ cursor: 'pointer' }}
-                        onClick={() => window.location.href = 'http://172.26.33.22:18081'}
+                        sx={{ cursor: 'pointer', padding: '0 14px' }}
+                        onClick={() => navigate('/report')}
                     >
-                        <IconSettings color={'white'} />
+                        <FontAwesomeIcon icon={faGear} size='lg' style={{ color: 'white' }} />
+                        {getFont('관리자 페이지')}
                     </Box>
                 )}
             </Box>
