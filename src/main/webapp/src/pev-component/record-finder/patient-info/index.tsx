e2a81f2dcf7e20d629713f97f065e9eed7766eca
clang-engineer <clang.engineer@gmail.com>
2026-01-10 01:11:13 +0900

    feat(ui): update patient info and record list layout

--- src/main/webapp/src/pev-component/record-finder/patient-info/index.tsx
+++ src/main/webapp/src/pev-component/record-finder/patient-info/index.tsx
@@ -1,17 +1,22 @@
-import { Paper } from "@mui/material";
-import React from "react"
-import MainCard from "ui-component/cards/MainCard"
-
 import { Grid } from "@mui/material";
 
 const PatientInfo = () => {
   return <Grid container spacing={1} padding={1} >
-    <Grid item xs={6}> 1 </Grid>
-    <Grid item xs={6}> 2 </Grid>
-    <Grid item xs={6}> 3 </Grid>
-    <Grid item xs={6}> 4 </Grid>
-    <Grid item xs={6}> 5 </Grid>
-    <Grid item xs={6}> 6 </Grid>
+    <Grid item xs={12} container>
+      <Grid item xs={6}>
+        등록번호 :
+      </Grid>
+      <Grid item xs={6}>
+        성명
+      </Grid>
+    </Grid>
+    <Grid item xs={12} container>
+      <Grid item xs={6}> 주민번호 </Grid>
+      <Grid item xs={6}> 성별/나이 </Grid>
+    </Grid>
+    <Grid item xs={12} container>
+      진료부서
+    </Grid>
   </Grid>
 
 }
--- src/main/webapp/src/pev-component/record-finder/record-list/index.tsx
+++ src/main/webapp/src/pev-component/record-finder/record-list/index.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import MainCard from 'ui-component/cards/MainCard';
+import SubCard from 'ui-component/cards/SubCard';
 
 const RecordList = () => {
-  return <div>Record List</div>;
+  return <MainCard title="Record List Component">
+    qqq
+  </MainCard>;
 };
 
 export default RecordList;
